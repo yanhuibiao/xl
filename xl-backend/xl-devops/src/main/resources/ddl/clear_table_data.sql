aa455105824f905fc8153a095f81368297d741ba
yanbeao <772383792@qq.com>
2025-07-17 00:14:23 +0800

    添加docker构建镜像文件

---
+++ xl-backend/xl-devops/src/main/resources/ddl/clear_table_data.sql
@@ -0,0 +1,15 @@
+TRUNCATE TABLE xl_account_0.trade_account_0;
+TRUNCATE TABLE xl_account_0.trade_account_1;
+TRUNCATE TABLE xl_order_0.trade_entry_0;
+TRUNCATE TABLE xl_order_0.trade_entry_1;
+TRUNCATE TABLE xl_order_0.trade_order_0;
+TRUNCATE TABLE xl_order_0.trade_order_1;
+
+TRUNCATE TABLE xl_account_0.trade_account_0;
+TRUNCATE TABLE xl_account_0.trade_account_1;
+TRUNCATE TABLE xl_order_1.trade_entry_0;
+TRUNCATE TABLE xl_order_1.trade_entry_1;
+TRUNCATE TABLE xl_order_1.trade_order_0;
+TRUNCATE TABLE xl_order_1.trade_order_1;
+
+TRUNCATE TABLE xl_users.customer;
